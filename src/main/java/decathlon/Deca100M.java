f349d86866d1b2d6a7be4d731e7c43318bc094a4
Staffan Åberg <staffan.aaberg@gmail.com>
2024-10-16 14:54:29 +0200

    Added class Competitor.java to keep track on Competitor data. Added InvalidResultException.java to throw an exception when too low/high values. Also added this exception in Deca100M.java. Added an export button and used the Competitor-class to add the result to excel. Added the Heptathlon events to the menu.

--- src/main/java/decathlon/Deca100M.java
+++ src/main/java/decathlon/Deca100M.java
@@ -4,41 +4,29 @@ import common.*;
 
 public class Deca100M {
 
-	private int score;
-	private double A = 25.4347;
-	private double B = 18;
-	private double C = 1.81;
-	boolean active = true;
-	CalcTrackAndField calc = new CalcTrackAndField();
-	InputResult inputResult = new InputResult();
+    private int score;
+    private double A = 25.4347;
+    private double B = 18;
+    private double C = 1.81;
+    boolean active = true;
+    CalcTrackAndField calc = new CalcTrackAndField();
+    InputResult inputResult = new InputResult();
 
-	// Calculate the score based on time. All running events.
-	public int calculateResult(double runningTime) {
+    // Calculate the score based on time. All running events.
+    public int calculateResult(double runningTime) throws InvalidResultException {
 
-		while (active) {
+        if (runningTime < 5) {
+            System.out.println("Value too low");
+            throw new InvalidResultException("Value too low");
+        } else if (runningTime > 20) {
+            System.out.println("Value too high");
+            throw new InvalidResultException("Value too high");
+        }
 
-			try {
-				// Acceptable values.
-				if (runningTime < 5) {
-					System.out.println("Value too low");
-					runningTime = inputResult.enterResult();
-				} else if (runningTime > 20) {
-					System.out.println("Value too high");
-					runningTime = inputResult.enterResult();
-				} else {
+        int score = calc.calculateTrack(A, B, C, runningTime);
 
-					score = calc.calculateTrack(A, B, C, runningTime);
-					active = false;
-				}
-			} catch (Exception e) {
+        System.out.println("The result is " + score);
 
-				System.out.println("Please enter numbers");
-			}
-		}
-		System.out.println("The result is " + score);
-
-		return score;
-
-	}
-
+        return score;
+    }
 }
