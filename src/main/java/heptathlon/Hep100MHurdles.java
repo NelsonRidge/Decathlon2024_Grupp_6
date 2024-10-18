161ebae189f65b5a4ef73ca02a167727ccfb81c4
jessica.petersen@utb.ecutbildning.se <jessica.petersen@utb.ecutbildning.se>
2024-10-18 22:11:37 +0200

    Bug fixes for input validation and changes to connected TestRunner tests

--- src/main/java/heptathlon/Hep100MHurdles.java
+++ src/main/java/heptathlon/Hep100MHurdles.java
@@ -1,43 +1,24 @@
 package heptathlon;
 
-import common.CalcTrackAndField;
-import common.InputResult;
+import common.*;
+import decathlon.InvalidResultException;
 
 public class Hep100MHurdles {
 
-	private int score;
-	private double A = 9.23076;
-	private double B = 26.7;
-	private double C = 1.835;
-	boolean active = true;
-	CalcTrackAndField calc = new CalcTrackAndField();
-	InputResult inputResult = new InputResult();
+    private double A = 9.23076;
+    private double B = 26.7;
+    private double C = 1.835;
+    CalcTrackAndField calc = new CalcTrackAndField();
 
-	// Calculate the score based on time. All running events.
-	public int calculateResult(double runningTime) {
+    // Calculate the score based on time. All running events.
+    public int calculateResult(double runningTime) throws InvalidResultException {
 
-		while (active) {
+        if (runningTime < 10) {
+            throw new InvalidResultException("Value too low");
+        } else if (runningTime > 30) {
+            throw new InvalidResultException("Value too high");
+        }
 
-			try {
-				// Acceptable values.
-				if (runningTime < 10) {
-					System.out.println("Value too low");
-					runningTime = inputResult.enterResult();
-				} else if (runningTime > 30) {
-					System.out.println("Value too high");
-					runningTime = inputResult.enterResult();
-				} else {
-
-					score = calc.calculateTrack(A, B, C, runningTime);
-					active = false;
-				}
-			} catch (Exception e) {
-
-				System.out.println("Please enter numbers");
-			}
-		}
-		System.out.println("The result is " + score);
-		return score;
-	}
-
+        return calc.calculateTrack(A, B, C, runningTime);
+    }
 }
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,9 +6,9 @@ import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class TestRunner {
-/*
+
     @Test
-    public void testDeca100validTime() {
+    public void testDeca100validTime() throws InvalidResultException {
         Deca100M deca100M = new Deca100M();
         int expected = 926;
         double testData = 10.71;
@@ -16,10 +16,8 @@ public class TestRunner {
         assertEquals(expected, actual);
     }
 
- */
-
     @Test
-    public void testDeca110MHurdlesValidTime() {
+    public void testDeca110MHurdlesValidTime() throws InvalidResultException {
         Deca110MHurdles deca110MHurdles = new Deca110MHurdles();
         int expected = 942;
         double testData = 14.25;
@@ -36,14 +34,15 @@ public class TestRunner {
         assertEquals(expected, actual);
     }
 
-     @Test
-      public void  testDeca1500MValidRunningTime() {
-          Deca1500M deca1500M = new Deca1500M();
-          int expected = 687;
-          double testData = 279;
-          int actual = deca1500M.calculateResult(testData);
-          assertEquals(expected, actual);
-      }
+    @Test
+    public void testDeca1500MValidRunningTime() {
+        Deca1500M deca1500M = new Deca1500M();
+        int expected = 687;
+        double testData = 279;
+        int actual = deca1500M.calculateResult(testData);
+        assertEquals(expected, actual);
+    }
+
     @Test
     public void testDecaDiscusThrowValidLength() {
         DecaDiscusThrow decaDiscusThrow = new DecaDiscusThrow();
@@ -52,58 +51,70 @@ public class TestRunner {
         int actual = decaDiscusThrow.calculateResult(testData);
         assertEquals(expected, actual);
     }
-    @Test public void  testDecaHighJumpValidHight() {
-        DecaHighJump decahighjump = new DecaHighJump() ;
+
+    @Test
+    public void testDecaHighJumpValidHight() {
+        DecaHighJump decahighjump = new DecaHighJump();
         int expected = 794;
         double testData = 199;
         int actual = decahighjump.calculateResult(testData);
         assertEquals(expected, actual);
     }
 
-    @Test public void  testDecaJavelinThrowValidLength() {
-        DecaJavelinThrow decaJavelinThrow = new DecaJavelinThrow() ;
+    @Test
+    public void testDecaJavelinThrowValidLength() {
+        DecaJavelinThrow decaJavelinThrow = new DecaJavelinThrow();
         int expected = 842;
         double testData = 66.87;
         int actual = decaJavelinThrow.calculateResult(testData);
-        assertEquals(expected, actual);    }
+        assertEquals(expected, actual);
+    }
 
-    @Test public void  testDecaLongJumpValidLength() {
-        DecaLongJump decaLongJump = new DecaLongJump() ;
+    @Test
+    public void testDecaLongJumpValidLength() throws InvalidResultException {
+        DecaLongJump decaLongJump = new DecaLongJump();
         int expected = 1010;
         double testData = 780;
         int actual = decaLongJump.calculateResult(testData);
         assertEquals(expected, actual);
     }
-    @Test public void  testDecaPoleVaultValidLength() {
-        DecaPoleVault decaPoleVault = new DecaPoleVault() ;
+
+    @Test
+    public void testDecaPoleVaultValidLength() throws InvalidResultException {
+        DecaPoleVault decaPoleVault = new DecaPoleVault();
         int expected = 1004;
         double testData = 530;
         int actual = decaPoleVault.calculateResult(testData);
         assertEquals(expected, actual);
     }
-    @Test public void  testDecaShotPutValidLength() {
-        DecaShotPut decaShotPut = new DecaShotPut() ;
+
+    @Test
+    public void testDecaShotPutValidLength() throws InvalidResultException {
+        DecaShotPut decaShotPut = new DecaShotPut();
         int expected = 805;
         double testData = 15.25;
         int actual = decaShotPut.calculateResult(testData);
         assertEquals(expected, actual);
     }
+
     @Test
-    public void testHep100MHurdlesValidTime() {
+    public void testHep100MHurdlesValidTime() throws InvalidResultException {
         Hep100MHurdles hep100MHurdles = new Hep100MHurdles();
         int expected = 1041;
         double testData = 13.56;
         int actual = hep100MHurdles.calculateResult(testData);
         assertEquals(expected, actual);
     }
+
     @Test
-    public void testHep200MValidTime() {
+    public void testHep200MValidTime() throws InvalidResultException {
         Hep200M hep200M = new Hep200M();
         int expected = 937;
         double testData = 24.46;
         int actual = hep200M.calculateResult(testData);
         assertEquals(expected, actual);
     }
+
     @Test
     public void testHep800MValidTime() {
         Hep800M hep800M = new Hep800M();
@@ -112,37 +123,40 @@ public class TestRunner {
         int actual = hep800M.calculateResult(testData);
         assertEquals(expected, actual);
     }
+
     @Test
-    public void  testHeptHightJumpValidHight() {
-        HeptHightJump heptHightJump = new HeptHightJump() ;
+    public void testHeptHightJumpValidHight() {
+        HeptHightJump heptHightJump = new HeptHightJump();
         int expected = 1132;
         double testData = 192;
         int actual = heptHightJump.calculateResult(testData);
         assertEquals(expected, actual);
     }
+
     @Test
-    public void  testHeptJavelinThrowValidLenght() {
-        HeptJavelinThrow heptJavelinThrow = new HeptJavelinThrow() ;
+    public void testHeptJavelinThrowValidLenght() {
+        HeptJavelinThrow heptJavelinThrow = new HeptJavelinThrow();
         int expected = 939;
         double testData = 54.04;
         int actual = heptJavelinThrow.calculateResult(testData);
         assertEquals(expected, actual);
     }
+
     @Test
-    public void  testHeptLongJumpValidLenght() {
-        HeptLongJump heptLongJump = new HeptLongJump() ;
+    public void testHeptLongJumpValidLenght() {
+        HeptLongJump heptLongJump = new HeptLongJump();
         int expected = 978;
         double testData = 641;
         int actual = heptLongJump.calculateResult(testData);
         assertEquals(expected, actual);
     }
+
     @Test
-    public void  testHeptShotPutValidLenght() {
-        HeptShotPut heptShotPut = new HeptShotPut() ;
+    public void testHeptShotPutValidLenght() {
+        HeptShotPut heptShotPut = new HeptShotPut();
         int expected = 897;
         double testData = 15.54;
         int actual = heptShotPut.calculateResult(testData);
         assertEquals(expected, actual);
     }
 }
-
